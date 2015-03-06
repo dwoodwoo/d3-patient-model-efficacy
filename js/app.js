0f95cff4071c88a5fe6415d708b3af7419511729
Dennis Woo <dwoo@sent.com>
2015-03-05 21:31:49 -0800

    redraw area2 based on click

--- js/app.js
+++ js/app.js
@@ -99,7 +99,7 @@ var circle = svg1.append("g")
     .on('mouseout', function(d) {tip.hide(d);
       // console.log(this);
      })
-    .on("click", function(){
+    .on("click", function(d, i){
             d3.select(this)
                 .transition()
                   .duration(750)
@@ -111,10 +111,10 @@ var circle = svg1.append("g")
                   .ease("bounce")
                   .attr('r', 10)
                   .style("fill", "steelblue");
-                  console.log("click event");
-                  if( $('#area2').is(':empty') ) {
-                    pie(piejson1);
-                  };
+                  console.log("click event d", d, "i", i);
+                  //manually empty #area2...pie.redraw() doesn't do it right
+                  document.getElementById("area2").innerHTML = "";
+                  var pie = new d3pie("area2", piejsonarr[i]);
                   
             // d3.select("#area2")
             //     .transition()
@@ -124,6 +124,7 @@ var circle = svg1.append("g")
     .call(tip);
 
 
+var piejsonarr = [];
 var piejson1 = {
   "header": {
     "title": {
@@ -204,8 +205,175 @@ var piejson1 = {
 };
 
 
+var piejson2 = {
+  "header": {
+    "title": {
+      "text": "30 Day Readmissions",
+      "fontSize": 24,
+      "font": "sans-serif"
+    },
+    "subtitle": {
+      "text": "Patient Characteristics, Illness Risk Factors, Hospital Course Risk Factors",
+      "color": "#999999",
+      "fontSize": 12,
+      "font": "sans-serif"
+    },
+    "titleSubtitlePadding": 9
+  },
+  "footer": {
+    "color": "#999999",
+    "fontSize": 10,
+    "font": "sans-serif",
+    "location": "bottom-left"
+  },
+  "size": {
+    "canvasWidth": 960,
+    "pieInnerRadius": "20%",
+  },
+  "data": {
+    "sortOrder": "",
+    "content": [
+      {"label":"One or more admissions in past year", "value": 6, "sign":"+", "color": "#5a84fa"},
+      {"label":"Married males", "value": 2, "sign": "+", "color": "#678dfb"},
+      {"label":"Medicare or Medi-Cal", "value": 2, "sign": "+", "color": "#7597fb"},
+      {"label":"Urgent admission", "value": 2, "sign": "+", "color": "#81a0fa"},
+      {"label":"Self-admit to ED", "value": 2, "sign": "+", "color": "#90abfb"},
+      {"label":"15 or more diagnoses", "value": 3, "sign": "+", "color": "#3b0300"},
+      {"label":"Any infectious disease", "value": 2, "sign": "+", "color": "#470500"},
+      {"label":"Any digestive disorder", "value": 2, "sign": "+", "color": "#510600"},
+      {"label":"Primary Dx of COPD", "value": 1, "sign": "+", "color": "#5c0702"},
+      {"label":"Musculoskeletal disorder", "value": 1, "sign": "-", "color": "#670901"},
+      {"label":"Current LOS 7 days or more", "value": 1, "sign": "+", "color": "#022c00"},
+      {"label":"Discharge to Hospice", "value": 2, "sign": "-", "color": "#043500"}
+    ]
+  },
+  "labels": {
+    "outer": {
+      "pieDistance": 32
+    },
+    "inner": {
+      "format": "value"
+    },
+    "mainLabel": {
+      "fontSize": 11
+    },
+    "percentage": {
+      "color": "#ffffff",
+      "decimalPlaces": 0
+    },
+    "value": {
+      "color": "#adadad",
+      "fontSize": 11
+    },
+    "lines": {
+      "enabled": true
+    }
+  },
+  "effects": {
+    "pullOutSegmentOnClick": {
+      "effect": "linear",
+      "speed": 400,
+      "size": 8
+    }
+  },
+  "misc": {
+    "gradient": {
+      "enabled": true,
+      "percentage": 100
+    }
+  }
+};
+
+var piejson3 = {
+  "header": {
+    "title": {
+      "text": "30 Day Readmissions",
+      "fontSize": 24,
+      "font": "sans-serif"
+    },
+    "subtitle": {
+      "text": "Patient Characteristics, Illness Risk Factors, Hospital Course Risk Factors",
+      "color": "#999999",
+      "fontSize": 12,
+      "font": "sans-serif"
+    },
+    "titleSubtitlePadding": 9
+  },
+  "footer": {
+    "color": "#999999",
+    "fontSize": 10,
+    "font": "sans-serif",
+    "location": "bottom-left"
+  },
+  "size": {
+    "canvasWidth": 960,
+    "pieInnerRadius": "20%",
+  },
+  "data": {
+    "sortOrder": "",
+    "content": [
+      {"label":"One or more admissions in past year", "value": 9, "sign":"+", "color": "#5a84fa"},
+      {"label":"Married males", "value": 2, "sign": "+", "color": "#678dfb"},
+      {"label":"Medicare or Medi-Cal", "value": 2, "sign": "+", "color": "#7597fb"},
+      {"label":"Urgent admission", "value": 2, "sign": "+", "color": "#81a0fa"},
+      {"label":"Self-admit to ED", "value": 1, "sign": "+", "color": "#90abfb"},
+      {"label":"15 or more diagnoses", "value": 1, "sign": "+", "color": "#3b0300"},
+      {"label":"Any infectious disease", "value": 2, "sign": "+", "color": "#470500"},
+      {"label":"Any digestive disorder", "value": 2, "sign": "+", "color": "#510600"},
+      {"label":"Primary Dx of COPD", "value": 1, "sign": "+", "color": "#5c0702"},
+      {"label":"Musculoskeletal disorder", "value": 1, "sign": "-", "color": "#670901"},
+      {"label":"Current LOS 7 days or more", "value": 1, "sign": "+", "color": "#022c00"},
+      {"label":"Discharge to Hospice", "value": 2, "sign": "-", "color": "#043500"}
+    ]
+  },
+  "labels": {
+    "outer": {
+      "pieDistance": 32
+    },
+    "inner": {
+      "format": "value"
+    },
+    "mainLabel": {
+      "fontSize": 11
+    },
+    "percentage": {
+      "color": "#ffffff",
+      "decimalPlaces": 0
+    },
+    "value": {
+      "color": "#adadad",
+      "fontSize": 11
+    },
+    "lines": {
+      "enabled": true
+    }
+  },
+  "effects": {
+    "pullOutSegmentOnClick": {
+      "effect": "linear",
+      "speed": 400,
+      "size": 8
+    }
+  },
+  "misc": {
+    "gradient": {
+      "enabled": true,
+      "percentage": 100
+    }
+  }
+};
+
+
+
+piejsonarr.push(piejson1);
+piejsonarr.push(piejson2);
+piejsonarr.push(piejson3);
+
+
+
 var pie = 
   function (piejson) {
     new d3pie("area2", piejson);
   };
+
 
